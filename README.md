28af93b1a6f078d77bf302ec342f6cbf84cf302a
darckronoz <crispipe14@gmail.com>
2022-11-17 23:30:05 -0500

    update to do and add some functions to Waiter

--- README.md
+++ README.md
@@ -4,6 +4,19 @@ python based simulation project with educational pourposes
 
 # TO - DO
 
+ - [ ] rename Client to Customer.
+ - [ ] rename ClientGroup to CustomerGroup.
  - [ ] develop module functions.
+	- [ ] Table.
+		- [ ] addCustomer().
+		- [ ] isEmpty().
+		- [ ] isShareable().
+		- [ ] getFreeSeats().
+	- [ ] Waiter.
+		- [x] assignTable().
+		- [x] findEmptyTable().
+		- [x] findShareableTable().
+	- [ ] CustomerGroup().
+		- [ ] getSize().
  - [ ] structure the restaurant (controller).
  - [ ] develop the data structures queue.
--- model/Waiter.py
+++ model/Waiter.py
@@ -11,6 +11,38 @@ class Waiter:
         self.tips = tips
         self.score = score
 
+    #this function assigns customers to tables and if all the tables are full
+    #then adds the customer to the tableQueue
+    def assignTable(self, customer, tableQueue):
+        table = self.findEmptyTable()
+        if table != False:
+            self.tables[table].addCustomer(customer)
+        else:
+            table = self.findTableToShare(customer.getSize())
+            if table != False:
+                self.tables[table].addCustomer(customer)
+            else:
+                tableQueue.push(customer)
+    
+    #Function that returns the first empty table on the table list (self.tables)
+    #if there's not empty tables returns False.
+    def findEmptyTable(self):
+        for table in self.tables:
+            if table.isEmpty() == True:
+                return table.id
+        return False
+
+    #function that returns the first shareable table on the list
+    #if the table is shareable but the group doesn't fit it isnt returned
+    #if there are not tables to share that meet the requirements (have enough space to the client group)
+    #returns False
+    def findTableToShare(self, groupLenght):
+        for table in self.tables:
+            if table.isShareable(): 
+                if table.getFreeSeats() <= groupLenght:
+                    return table.id
+        return False
+
 
 
 
