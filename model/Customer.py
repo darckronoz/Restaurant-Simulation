fc66ab6a67ced811442a06d933445257954ba36e
davidU01 <david.abril@uptc.edu.co>
2022-11-21 22:02:55 -0500

    Cambios en cliente y en grupo

--- model/Customer.py
+++ model/Customer.py
@@ -1,8 +1,11 @@
+import numpy as np
 
 class Customer:
 
-    def __init__(self, table, tip, share_table, capacity, order, plate_score, waiter_score):
-        self.table = table
+    #El cliente cuenta con un Id, Propina, compartir mesa, capacidad, pedido, puntaje del plato y puntaje del mesero
+
+    def __init__(self, customer_id, tip, share_table, capacity, order, plate_score, waiter_score ):
+        self.customer_id = customer_id
         self.tip = tip
         self.share_table = share_table
         self.capacity = capacity
@@ -10,7 +13,9 @@ class Customer:
         self.plate_score = plate_score
         self.waiter_score = waiter_score
 
-    
+
+
+
 
 
         
