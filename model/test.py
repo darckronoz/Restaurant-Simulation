242370c0f636699a854472c14f8477f1e03c217d
darckronoz <crispipe14@gmail.com>
2022-11-23 20:18:11 -0500

    test

--- model/test.py
+++ model/test.py
@@ -1,5 +1,6 @@
 
 from Kitchen import Kitchen as k
+from Order import Order as o
 import numpy as np
 
 #importing and creating object test
@@ -11,4 +12,16 @@ i = np.array([0, 0])
 b = np.arange(5)
 i = np.concatenate((i, b))
 print(i)
+
+lol = o('holsd','dsa')
+lol1 = o('holsd','dsa')
+lol2 = o('holsd','dsa')
+
+j = [lol, lol1, lol2]
+
+k = np.array(j)
+
+print(k)
+
+
 
