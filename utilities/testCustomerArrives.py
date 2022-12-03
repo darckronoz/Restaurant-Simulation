3f6c7a2c2909ad08b58dee6dfbc3f78bc1873521
darckronoz <crispipe14@gmail.com>
2022-12-03 17:38:33 -0500

    customer distribution ended

--- utilities/testCustomerArrives.py
+++ utilities/testCustomerArrives.py
@@ -1,24 +1,33 @@
-from multiprocessing import Process
+import ctypes
+import multiprocessing
+import sys
 
 from Utilities import generate_big_ri
 from Utilities import get_arrival_time
 import time
 
-global days
-global hours
-global minutes
-global seconds
+days = multiprocessing.Value(ctypes.c_int, 0)
+hours = multiprocessing.Value(ctypes.c_int, 0)
+#minutes = multiprocessing.Value(ctypes.c_int, 0)
+#seconds = multiprocessing.Value(ctypes.c_int, 0)
 
 customer_ri = generate_big_ri(30000)
 
 
 def customer_distribution():
+    customer_counter = 0
+    t = multiprocessing.Process(target=timer)
+    t.start()
+    time.sleep(1)
     customer_arrives()
-    print(get_timer())
-    for t in customer_ri:
-        time.sleep(get_arrival_time((180 / 7), t))
+    for time_arrive in customer_ri:
+        x = get_arrival_time((3/7), time_arrive)
+        customer_counter += 1
+        time.sleep(x/60)
         customer_arrives()
-        print(get_timer())
+        print(customer_counter)
+        if not t.is_alive():
+            sys.exit(0)
 
 
 def customer_arrives():
@@ -26,28 +35,20 @@ def customer_arrives():
 
 
 def timer():
-    global days
-    global hours
-    global minutes
-    global seconds
-    seconds, hours, minutes, days = 0, 0, 0, 0
-    while days < 1:
-        time.sleep(1 / 60)
-        seconds += 1
-        if seconds == 60:
-            seconds = 0
-            minutes += 1
-        if minutes == 60:
-            minutes = 0
-            hours += 1
-        if hours == 24:
-            hours = 0
-            days += 1
+    global seconds, hours, minutes, days
+    while days.value < 1:
+        time.sleep(1)
+        hours.value += 1
+        if hours.value == 24:
+            hours.value = 0
+            days.value += 1
+        print(('d: ', days.value, 'h: ', hours.value))
 
 
 def get_timer():
-    return str(('d: ', days, 'h: ', hours, 'm: ', minutes, 's: ', seconds))
+    return str(('d: ', days.value, 'h: ', hours.value, 'm: ', minutes.value, 's: ', seconds.value))
 
 
-
-
+if __name__ == '__main__':
+    c = multiprocessing.Process(target=customer_distribution)
+    c.start()
